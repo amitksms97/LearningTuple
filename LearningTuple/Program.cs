f62bc097edd369e8c9cef81b193114ef735ac5af
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:13:01 +0530

    Add project files.

---
+++ LearningTuple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tuple<int, String, bool> myTuple = new Tuple<int, String, bool>(1, "hello", true);
+
+            var myTuple = Tuple.Create(1, "hello", true);
+
+            Console.WriteLine(myTuple.Item2);
+        }
+    }
+}
